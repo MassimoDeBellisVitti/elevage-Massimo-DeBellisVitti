f2be1d2f0227ed68ffc37b3cdcb9f55e053d8869
Massimo De Bellis Vitti <debellisvitti.massimo@gmail.com>
2025-04-09 15:01:05 +0200

    model elevage created

--- elevage/game/models.py
+++ elevage/game/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Elevage(models.Model):
+    
+    name = models.CharField(max_length=100)
+    male_rabbits = models.IntegerField()
+    female_rabbits = models.IntegerField()
+    foodLevel = models.IntegerField()
+    cageNumber = models.IntegerField()
+    money = models.IntegerField()
+    month = models.IntegerField(default=0)
+    
 
+    def __str__(self):
+        return self.name
 
 # Create your models here.
